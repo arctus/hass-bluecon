074bc7f472147840e9d1c43dd3aac90f7e56b62e
Pavel Cuchriajev <pavel.cuchriajev@appscard.com>
2025-04-09 16:53:42 +0200

    FIX: Accidental push.

--- custom_components/bluecon/bluecon/bluecon/model/Pairing.py
+++
@@ -1,9 +0,0 @@
-from bluecon.model.AccessDoor import AccessDoor
-
-
-class Pairing:
-    def __init__(self, pairingResponseDictionary: dict):
-        self.id = pairingResponseDictionary['id']
-        self.deviceId = pairingResponseDictionary['deviceId']
-        self.accessDoorMap = {k: AccessDoor(v) for k, v in pairingResponseDictionary['accessDoorMap'].items() if v['visible']}
-        pass
--- custom_components/bluecon/bluecon/bluecon/model/User.py
+++
@@ -1,4 +0,0 @@
-class User:
-    def __init__(self, userResponseDictionary: dict):
-        self.email = userResponseDictionary['email']
-        self.locale = userResponseDictionary['locale']
